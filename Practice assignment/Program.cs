979cdc977f245278e9ad6887979f44cd4362ccdc
Anothile <st10440981@vccconnect.edu.za>
2026-04-20 20:07:24 +0200

    Program.cs

--- Practice assignment/Program.cs
+++ Practice assignment/Program.cs
@@ -1,33 +1,4 @@
-/*var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
-*/
 using Microsoft.EntityFrameworkCore;
 using Practice_assignment.Data;
 using Practice_assignment.Patterns.Factory;
@@ -42,21 +13,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Repositories (Repository Pattern)
+// Repositories: Repository Pattern
 builder.Services.AddScoped<IContractRepository, ContractRepository>();
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
 builder.Services.AddScoped<IServiceRequestRepository, ServiceRequestRepository>();
 
-// Factories (Factory Pattern) 
+// Factories : Factory Pattern
 builder.Services.AddSingleton<IContractFactory, ContractFactory>();
 
-// Observers (Observer Pattern) 
+// Observers: Observer Pattern
 builder.Services.AddScoped<IContractObserver, AuditLogObserver>();
 
 // Business Logic Services 
 builder.Services.AddScoped<IContractService, ContractService>();
 builder.Services.AddScoped<IServiceRequestService, ServiceRequestService>();
 builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<IContractObserver, AuditLogObserver>();
+builder.Services.AddScoped<IContractObserver, ExpiryNotificationObserver>(); 
 
 // HTTP Client for Currency API 
 builder.Services.AddHttpClient<ICurrencyService, CurrencyService>(client =>
@@ -85,7 +58,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// Auto-migrate on startup (dev convenience) 
+// Auto migrate on startup 
 using var scope = app.Services.CreateScope();
 var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 db.Database.Migrate();
